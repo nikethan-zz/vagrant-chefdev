26b3e30adb561f941849f4a3b7e7ed21d5a25e04
nikethan.nagularaja <nikethan.nagularaja@workday.com>
2013-12-27 17:41:18 -0800

    first commit

---
+++ lib/vagrant/chefdev.rb
@@ -0,0 +1,7 @@
+require "vagrant/chefdev/version"
+
+module Vagrant
+  module Chefdev
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/chefdev/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Chefdev
+    VERSION = "0.0.1"
+  end
+end
